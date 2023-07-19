0378e11d6603f5c3124b43e6ce1235c162d51574
catherine0305 <catherinemlay03@gmail.com>
2023-07-19 06:27:04 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 	while (i < 50)
 	{
 		if (i == 1)
-			printf("%d", d);
+			printf("%ld", d);
 		else if (i == 2)
 			printf(", %d", j);
 		else 
@@ -22,6 +22,6 @@ int main(void)
 		}
 		++i;
 	}
-	printf('\n');
+	_putchar('\n');
 	return (0);
 }
