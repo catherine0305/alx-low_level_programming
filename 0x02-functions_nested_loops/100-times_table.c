7bc3b3fb033beae3893c08833f78eae61d7655f2
catherine0305 <catherinemlay03@gmail.com>
2023-07-19 05:11:22 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,38 +11,28 @@ void print_times_table(int n)
 	{
 	for (l = 0; l <= n; l++)
 	{
-	for (m = 0; m <= n; m++)
+	_putchar('0');
+	for (m = 1; m <= n; m++)
 	{
+	_putchar(',');
+	_putchar(' ');
 	p = m * l;
-	if (m == 0)
+	if (p <= 99)
+	_putchar(' ');
+	if (p <= 0)
+	_putchar(' ');
+	if (p >= 100)
 	{
-		_putchar(p + '0');
-	}
-	else if (p < 10 && m != 0)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(p + '0');
-	}
-	else if (p >= 10 && p < 100)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar((p / 10) + '0');
-		_putchar((p % 10) + '0');
-	}
-	else if (p >= 100)
-	{
-		_putchar(',');
-		_putchar(' ');
 		_putchar((p / 100) + '0');
-		_putchar(((p / 10) % 10) + '0');
-		_putchar((p % 10) + '0');
+		_putchar(((p / 10)) % 10 + '0');
 	}
+	else if (p <= 99 && p >= 10)
+	{
+	_putchar((p / 10) + '0');
 	}
+	_putchar((p % 10) + '0');
+	}
+	_putchar('\n');
 	}
 	}
 }
