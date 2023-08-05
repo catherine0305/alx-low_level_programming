2731823aede15eef2034258021c04afa7423a493
catherine0305 <catherinemlay03@gmail.com>
2023-08-05 12:01:34 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase char
+ * @c: the char to check
+ * Return: 1 if lowercase and 0 if otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
