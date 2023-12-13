7d9698b7aaa2bb57a57aff29bcb38dccba62527a
catherine0305 <catherinemlay03@gmail.com>
2023-12-13 05:57:47 -0800

    dynamic lib

---
+++ 0x18-dynamic_libraries/functions/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic char
+ * @c: char to be checked
+ * Return: 1 if c is a letter and 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
